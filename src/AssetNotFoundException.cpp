9aaeb9aece2bbac241b810fbdb4d7cac52f36025
Heiko van der Heijden <heikovanderheijden@Heikos-MacBook-Pro.local>
2015-12-27 17:16:41 +0100

    woops

--- src/AssetNotFoundException.cpp
+++
@@ -1,9 +0,0 @@
-#include "AssetNotFoundException.hpp"
-
-AssetNotFoundException::AssetNotFoundException(std::string msg){
-    this->message = msg;
-}
-
-std::string AssetNotFoundException::msg(){
-    return this->message;
-}
